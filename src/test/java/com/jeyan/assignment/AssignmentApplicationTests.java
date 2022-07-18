bf1ece8f34f7506aa1ea463f4d603bd520c79783
JegatheeswarnJeyan <90918817+JegatheeswarnJeyan@users.noreply.github.com>
2022-07-18 17:53:26 +0530

    Initial commit

---
+++ src/test/java/com/jeyan/assignment/AssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jeyan.assignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssignmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
